2a6307f3e40e7fde1f9174d78682ee8ae9594ba1
Michele Mari <marimichele26@gmail.com>
2023-08-24 10:38:08 +0200

    progress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4173',
-    supportFile: 'src/tests/cypress/support/e2e.{js,jsx,ts,tsx}',
-    specPattern: 'src/tests/cypress/e2e/*.cy.{js,jsx,ts,tsx}',
-    screenshotsFolder: 'src/tests/cypress/screenshots',
-    videosFolder: 'src/tests/cypress/videos'
+    baseUrl: 'http://localhost:4173'
   },
 })
